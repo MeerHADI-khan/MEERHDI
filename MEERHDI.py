45db598a8cfea2b466636534a72802c189271207
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-22 13:58:22 +0500

    MEERHDI.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf MEERHDI
    git clone --depth=1 https://github.com/MeerHADI-khan/MEERHDI.git
    cd MEERHADI
    python MEERHDI.py

---
+++ MEERHDI.py
@@ -0,0 +1,32 @@
+# SAMI
+Paid___ Tools
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-MR.HADI-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/MEERHDI.git)<br> [![Facebook](https://img.shields.io/badge/Facebook-HADI-blue?style=flat-square&logo=facebook)](https://www.facebook.com/MR.SAMI.XD)<br> [![Whatsapp](https://img.shields.io/badge/whatsapp-MRHADI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [MR.HADI-dimgray]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf MRHDI--`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/MEERHDI.git`
+- `cd MEERHDI`
+- `python MRHDI-.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-AKING-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011
